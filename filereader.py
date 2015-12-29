bd64530b7187bec1d72ff2db0003e065a8ebd7b0
salty <656729195@qq.com>
2015-12-30 01:17:48 +0800

    new code

---
+++ filereader.py
@@ -0,0 +1,57 @@
+from numpy import *
+
+def readfile(filename):
+    f = open(filename)
+    attrname = f.readline().strip().split(',')
+    #determine the types of all attributes, i.e., numeric (0), categorical (1) and response (2)
+    attrtype = analyzetype(attrname)
+    numattrnum = 0
+    cateattrnum = 0
+    for t in attrtype:
+        if t == 0:
+            numattrnum += 1
+        elif t == 1:
+            cateattrnum += 1
+    istrainingfile = True if attrtype[-1] == 2 else False
+    data = f.readlines()
+    numattr = zeros((len(data), numattrnum))
+    cateattr = []
+    if istrainingfile:
+        label = []
+    index = 0
+    for line in data:
+        attrvalues = line.strip().split(',')
+        i, j = 0, 0
+        k = 0
+        singlecateattr = []
+        while i < numattrnum and j < cateattrnum:
+            val = attrvalues[k]
+            if attrtype[k] == 0:
+                numattr[index][i] = float(val) if val != '' else 0.0
+                i += 1
+            else:
+                singlecateattr.append(val)
+                j += 1
+            k += 1
+        cateattr.append(singlecateattr)
+        if istrainingfile:
+            label.append(attrvalues[-1])
+        index += 1
+    if istrainingfile:
+        return numattr, cateattr, label
+    else:
+        return numattr, cateattr
+
+def analyzetype(attrname):
+    numattrstr = 'Product_Info_4, Ins_Age, Ht, Wt, BMI, Employment_Info_1, Employment_Info_4, Employment_Info_6, Insurance_History_5, Family_Hist_2, Family_Hist_3, Family_Hist_4, Family_Hist_5, Medical_History_1, Medical_History_10, Medical_History_15, Medical_History_24, Medical_History_32'
+    numattrnameset = set(numattrstr.split(', '))
+    attrtype = []
+    for t in attrname:
+        name = t[1:-1]
+        if name in numattrnameset:
+            attrtype.append(0)
+        elif name == 'response':
+            attrtype.append(2)
+        else:
+            attrtype.append(1)
+    return attrtype
